f26d1ac6dfafb535006c70be6e4b55e369c637e7
Norris Nakagaki <norrisnakagaki@gmail.com>
2017-12-04 11:24:39 -0500

    use the html

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => path.join(__dirname+'/public/index.html'))
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
